e1651d97ef29eb3fed0ecb5d08cee9887c487d57
IrinaSemGB <irina.semyachkina@mail.ru>
2022-01-06 19:37:13 +0300

    Created app's interface and checkbox state

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todoey/screens/tasks_screen.dart';
+
+void main() => runApp(TodoApp());
+
+class TodoApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TasksScreen(),
+    );
+  }
+}
+
+
+
